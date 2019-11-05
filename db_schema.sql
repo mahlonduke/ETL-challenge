a752f05561c7c4e5d7c4972e08dc097f0f6f6dc6
mahlonduke <53539373+mahlonduke@users.noreply.github.com>
2019-11-04 21:15:32 -0800

    Updated db_schema.sql with additional tables for each data source

--- db_schema.sql
+++ db_schema.sql
@@ -1,4 +1,44 @@
-CREATE TABLE IF NOT EXISTS coffee (
+CREATE TABLE IF NOT EXISTS coffeebeancorral (
+	id INT PRIMARY KEY,
+	name TEXT,
+	price FLOAT,
+	star_rating FLOAT,
+	source TEXT,
+	url TEXT
+	);
+
+
+CREATE TABLE IF NOT EXISTS freshroastedcoffee (
+	id INT PRIMARY KEY,
+	name TEXT,
+	price FLOAT,
+	star_rating FLOAT,
+	source TEXT,
+	url TEXT
+	);
+
+
+CREATE TABLE IF NOT EXISTS amazon (
+	id INT PRIMARY KEY,
+	name TEXT,
+	price FLOAT,
+	star_rating FLOAT,
+	source TEXT,
+	url TEXT
+	);
+
+
+CREATE TABLE IF NOT EXISTS deansbeans (
+	id INT PRIMARY KEY,
+	name TEXT,
+	price FLOAT,
+	star_rating FLOAT,
+	source TEXT,
+	url TEXT
+	);
+
+
+CREATE TABLE IF NOT EXISTS coffee_combined (
 	id INT PRIMARY KEY,
 	name TEXT,
 	price FLOAT,
